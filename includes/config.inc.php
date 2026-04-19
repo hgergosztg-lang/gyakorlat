6b0fb5b0a06097a43faea8492eb66fe863867c16
hgergosztg-lang <hgergosztg@gmail.com>
2026-04-19 20:36:00 +0200

    Add new pages and error handling to config
    Added new pages for login, logout, and registration, along with a custom error page.

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 $ablakcim = array(
-    'cim' => 'Választott Téma Megnevezése',
+    'cim' => 'Utazás',
 );
 
 $fejlec = array(
@@ -24,3 +24,12 @@ $oldalak += array(
     'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)),
     'crud' => array('fajl' => 'crud', 'szoveg' => 'Adatkezelés', 'menun' => array(1,1))
 );
+$oldalak += array(
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+
+$hiba_oldal = array('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
