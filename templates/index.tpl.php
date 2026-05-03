1481495d5a027a092c25ea6d6304937bffb9503b
RF8DM1 <mecseryzeno03@gmail.com>
2026-05-03 20:45:06 +0200

    nethelyre való beállítások az eléréshez!

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,42 +1,55 @@
 <?php session_start(); ?>
 <?php if(file_exists('./logicals/'.$keres['fajl'].'.php')) { include("./logicals/{$keres['fajl']}.php"); } ?>
 <!DOCTYPE html>
-<html>
+<html lang="hu">
 <head>
-	<meta charset="utf-8">
-	<title><?= $ablakcim['cim'] . ( (isset($ablakcim['mottó'])) ? ('|' . $ablakcim['mottó']) : '' ) ?></title>
-	<link rel="stylesheet" href="./styles/stilus.css" type="text/css">
-	<?php if(file_exists('./styles/'.$keres['fajl'].'.css')) { ?><link rel="stylesheet" href="./styles/<?= $keres['fajl']?>.css" type="text/css"><?php } ?>
+    <meta charset="utf-8">
+    <title><?= $ablakcim['cim'] . ( (isset($ablakcim['mottó'])) ? (' | ' . $ablakcim['mottó']) : '' ) ?></title>
+    <link rel="stylesheet" href="./styles/stilus.css" type="text/css">
+    <?php if(file_exists('./styles/'.$keres['fajl'].'.css')) { ?>
+        <link rel="stylesheet" href="./styles/<?= $keres['fajl']?>.css" type="text/css">
+    <?php } ?>
 </head>
 <body>
-	<header>
-		<img src="./images/<?=$fejlec['kepforras']?>" alt="<?=$fejlec['kepalt']?>">
-		<h1><?= $fejlec['cim'] ?></h1>
-		<?php if (isset($fejlec['motto'])) { ?><h2><?= $fejlec['motto'] ?></h2><?php } ?>
-		<?php if(isset($_SESSION['login'])) { ?>Bejlentkezve: <strong><?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></strong><?php } ?>
-	</header>
+    <header>
+        <img src="./images/<?=$fejlec['kepforras']?>" alt="<?=$fejlec['kepalt']?>">
+        <h1><?= $fejlec['cim'] ?></h1>
+        <?php if (isset($fejlec['motto'])) { ?><h2><?= $fejlec['motto'] ?></h2><?php } ?>
+        <?php if(isset($_SESSION['login'])) { ?>
+            <div class="login-info">Bejelentkezve: <strong><?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></strong></div>
+        <?php } ?>
+    </header>
     <div id="wrapper">
         <aside id="nav">
             <nav>
                 <ul>
-					<?php foreach ($oldalak as $url => $oldal) { ?>
-						<?php if(! isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
-							<li<?= (($oldal == $keres) ? ' class="active"' : '') ?>>
-							<a href="<?= ($url == '/') ? '.' : $url ?>">
-							<?= $oldal['szoveg'] ?></a>
-							</li>
-						<?php } ?>
-					<?php } ?>
+                    <?php foreach ($oldalak as $url => $oldal) { ?>
+                        <?php if(!isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
+                            <li<?= (($oldal == $keres) ? ' class="active"' : '') ?>>
+                                <!-- A javított link: ha a főoldal (/), akkor pont, egyébként kérdőjel + url -->
+                                <a href="<?= ($url == '/') ? '.' : ('?' . $url) ?>">
+                                    <?= $oldal['szoveg'] ?>
+                                </a>
+                            </li>
+                        <?php } ?>
+                    <?php } ?>
                 </ul>
             </nav>
         </aside>
         <div id="content">
-            <?php include("./templates/pages/{$keres['fajl']}.tpl.php"); ?>
+            <?php 
+            $fajl = "./templates/pages/{$keres['fajl']}.tpl.php";
+            if(file_exists($fajl)) {
+                include($fajl); 
+            } else {
+                echo "Hiba: A tartalom nem található!";
+            }
+            ?>
         </div>
     </div>
     <footer>
         <?php if(isset($lablec['copyright'])) { ?>&copy;&nbsp;<?= $lablec['copyright'] ?> <?php } ?>
-		&nbsp;
+        &nbsp;
         <?php if(isset($lablec['ceg'])) { ?><?= $lablec['ceg']; ?><?php } ?>
     </footer>
 </body>
