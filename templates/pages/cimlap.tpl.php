0558b925bc44bb7bb788a0b00d5c564d35cc84f6
hgergosztg-lang <hgergosztg@gmail.com>
2026-05-09 23:27:20 +0200

    Update cimlap.tpl.php

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -1,12 +1,12 @@
 <h2>Üdvözöljük a Napfény Tours oldalán!</h2>
 
-<img src="./images/arc.jpg" alt="Üdvözlés" style="float:right; margin:10px; width:250px;">
+<img src="./images/arc.jpg" alt="Üdvözlés" style="float:right; margin:10px; width:250px; border-radius: 8px;">
 
 <div class="tartalom">
     <div style="margin-top: 20px; margin-bottom: 20px;">
         <div style="display:inline-block; vertical-align:top; margin-right:20px;">
             <h3>Bemutató videónk</h3>
-            <video width="320" height="240" controls>
+            <video width="320" height="240" controls style="border: 1px solid #ccc; background: #000;">
                 <source src="./videos/sajat_video.mp4" type="video/mp4">
                 A böngészője nem támogatja a videó lejátszását.
             </video>
@@ -14,33 +14,32 @@
 
         <div style="display:inline-block; vertical-align:top;">
             <h3>Ajánlott úti célunk</h3>
-            <iframe width="320" height="240" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+            <iframe width="320" height="240" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen style="border: 1px solid #ccc;"></iframe>
         </div>
     </div>
 
     <div class="leiras">
         <h3>A Napfény Toursról</h3>
         <p>
-            A Napfény Tours utazási iroda internetes foglalási rendszere segítségével böngészhet 2011-es tavaszi ajánlataink között. 
+            A Napfény Tours utazási iroda internetes foglalási rendszere segítségével böngészhet ajánlataink között. 
             Kínálatunkban Tunézia és Egyiptom legszebb tájai szerepelnek.
-        </p>
-        <p>
-            Válasszon Sousse, Djerba, Sharm El Sheikh vagy Hurghada kiváló szállodái közül! Ügyfélszolgálatunk készséggel áll rendelkezésére minden kérdésben.
         </p>
     </div>
 
     <div style="margin-top: 30px;">
         <h3>Irodánk elhelyezkedése</h3>
-        <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2725.3344335606!2d19.6669509!3d46.8960799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743da7a6c479e1d%3A0xc8292b3f6dc69e7f!2sPallasz%20Ath%C3%A9n%C3%A9%20Egyetem%20GAMF%20Kar!5e0!3m2!1shu!2shu!4v1619520000000!5m2!1shu!2shu" 
-            width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy">
-        </iframe>
+        <div style="width: 100%"><iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&height=300&hl=hu&q=Kecskem%C3%A9t,%20Izs%C3%A1ki%20%C3%BAt%2010+(Napf%C3%A9ny%20Tours%20-%20GAMF)&t=&z=15&ie=UTF8&iwloc=B&output=embed"></iframe></div>
         <br>
         <small>
-            <a target="_blank" href="https://www.google.hu/maps/place/Pallasz+Ath%C3%A9n%C3%A9+Egyetem+GAMF+Kar/@46.8960799,19.6669509,17z/data=!3m1!4b1!4m5!3m4!1s0x4743da7a6c479e1d:0xc8292b3f6dc69e7f!8m2!3d46.8960763!4d19.6691396?hl=hu">
+            <a target="_blank" href="https://www.google.hu/maps/place/Neumann+J%C3%A1nos+Egyetem+GAMF+Kar/@46.8960763,19.6691396,17z/">
                 Nagyobb térkép megnyitása
             </a>
         </small>
+    </div>
+
+    <hr style="margin-top: 40px; border: 0; border-top: 1px solid #eee;">
+    <div style="text-align: right; font-style: italic; color: #555; margin-top: 10px;">
+        <p>Készítette: <strong>Horváth Gergő (H8ASDU)</strong> és <strong>Mecséry Zenó (RF8DM1)</strong></p>
     </div>
 </div>
 
