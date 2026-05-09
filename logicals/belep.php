edb5f6d8fc76c829ae1cf4e945019b79af01b718
hgergosztg-lang <hgergosztg@gmail.com>
2026-05-09 23:16:45 +0200

    Update belep.php

--- logicals/belep.php
+++ logicals/belep.php
@@ -1,18 +1,21 @@
 <?php
 if(isset($_POST['felhasznalo']) && isset($_POST['jelszo'])) {
     try {
-        // Kapcsolódás
-        $dbh = new PDO('mysql:host=localhost;dbname=gyakorlat7', 'root', '',
-                        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $dbh = new PDO("mysql:host=localhost;dbname=gamf_admin", "gamf_admin", "Gamf123.",
+                array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
-        // Felhsználó keresése
-        $sqlSelect = "select id, csaladi_nev, uto_nev from felhasznalok where bejelentkezes = :bejelentkezes and jelszo = sha1(:jelszo)";
+        // Figyelj: itt utonev (nincs aláhúzás)
+        $sqlSelect = "select id, csaladi_nev, utonev from felhasznalok where bejelentkezes = :bejelentkezes and jelszo = sha1(:jelszo)";
         $sth = $dbh->prepare($sqlSelect);
         $sth->execute(array(':bejelentkezes' => $_POST['felhasznalo'], ':jelszo' => $_POST['jelszo']));
         $row = $sth->fetch(PDO::FETCH_ASSOC);
+        
         if($row) {
-            $_SESSION['csn'] = $row['csaladi_nev']; $_SESSION['un'] = $row['uto_nev']; $_SESSION['login'] = $_POST['felhasznalo'];
+            // Itt javítottam: $row['utonev']-et kapunk az SQL-ből
+            $_SESSION['csn'] = $row['csaladi_nev']; 
+            $_SESSION['un'] = $row['utonev']; 
+            $_SESSION['login'] = $_POST['felhasznalo'];
         }
     }
     catch (PDOException $e) {
