4e84cce5594d11f5a0e8b509ad34b3239a884a9b
hgergosztg-lang <hgergosztg@gmail.com>
2026-05-09 23:45:53 +0200

    Update config.inc.php

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -15,23 +15,16 @@ $lablec = array(
     'ceg' => 'Napfény Tours Utazási Iroda'
 );
 
-/* Oldalak beállítása:
-  'url' => array('fajl', 'szoveg', array(látható_kijelentkezve, látható_bejelentkezve))
-*/
 $oldalak = array(
     '/' => array('fajl' => 'cimlap', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
     'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
     'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képgaléria', 'menun' => array(1,1)),
     'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)),
-    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Adatkezelés (CRUD)', 'menun' => array(1,1))
-);
-
-// Technikai oldalak (Menüben nem látszanak, de az útvonalválasztónak ismernie kell őket)
-$oldalak += array(
+    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Adatkezelés (CRUD)', 'menun' => array(1,1)),
     'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => 'Regisztráció', 'menun' => array(1,0)),
     'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
     'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
-    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0)),
     'uzenet_kuldes' => array('fajl' => 'uzenet_kuldes', 'szoveg' => '', 'menun' => array(0,0)),
     'helyseg_szerkeszt' => array('fajl' => 'helyseg_szerkeszt', 'szoveg' => '', 'menun' => array(0,0)),
     'helyseg_mentes' => array('fajl' => 'helyseg_mentes', 'szoveg' => '', 'menun' => array(0,0))
