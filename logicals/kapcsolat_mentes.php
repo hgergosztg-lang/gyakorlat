20199b55e73f5948e098b463fddd4eec7874c99e
hgergosztg-lang <hgergosztg@gmail.com>
2026-05-09 23:19:13 +0200

    Update kapcsolat_mentes.php

--- logicals/kapcsolat_mentes.php
+++ logicals/kapcsolat_mentes.php
@@ -1,13 +1,13 @@
 <?php
 $reakcio = "";
 if(isset($_POST['nev']) && isset($_POST['email']) && isset($_POST['szoveg'])) {
-    // Szerveroldali ellenőrzés (HTML ellenőrzés tilos!) [cite: 36]
+
     if(strlen($_POST['nev']) < 3 || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) || strlen($_POST['szoveg']) < 10) {
         $reakcio = "Hibás adatok! Kérjük, töltsön ki minden mezőt megfelelően.";
     } else {
         try {
-            $dbh = new PDO('mysql:host=localhost;dbname=gyakorlat7', 'root', '',
-                            array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+            $dbh = new PDO("mysql:host=localhost;dbname=gamf_admin", "gamf_admin", "Gamf123.",
+                array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
             $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
             
             $sqlInsert = "insert into uzenetek(id, nev, email, idopont, szoveg) 
