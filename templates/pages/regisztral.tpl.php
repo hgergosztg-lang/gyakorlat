fd4e19f98275082476ca4629e75b6711a7d306e5
hgergosztg-lang <hgergosztg@gmail.com>
2026-05-09 23:37:47 +0200

    Update regisztral.tpl.php

--- templates/pages/regisztral.tpl.php
+++ templates/pages/regisztral.tpl.php
@@ -1,15 +1,25 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Regisztráció</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <?php if(isset($uzenet)) { ?>
-            <h1><?= $uzenet ?></h1>
-            <?php if($ujra) { ?>
-                <a href="belepes">Próbálja újra!</a>
-            <?php } ?>
+<h2>Regisztráció</h2>
+
+<?php if(isset($uzenet)) { ?>
+    <div style="background: #f0f0f0; padding: 10px; border: 1px solid #ccc;">
+        <h3><?= $uzenet ?></h3>
+        <?php if(!$ujra) { ?>
+            <p><a href="?belepes">Kattintson ide a belépéshez!</a></p>
         <?php } ?>
-    </body>  
-</html>
+    </div>
+<?php } ?>
+
+<?php if(!isset($uzenet) || (isset($ujra) && $ujra)) { ?>
+    <form action="index.php?regisztral" method="post">
+        <fieldset>
+            <legend>Adatok megadása</legend>
+            <br>
+            <input type="text" name="vezeteknev" placeholder="Vezetéknév"><br><br>
+            <input type="text" name="utonev" placeholder="Utónév"><br><br>
+            <input type="text" name="felhasznalo" placeholder="Felhasználói név"><br><br>
+            <input type="password" name="jelszo" placeholder="Jelszó"><br><br>
+            <input type="submit" name="regisztracio" value="Regisztráció">
+            <br>&nbsp;
+        </fieldset>
+    </form>
+<?php } ?>
